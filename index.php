c324e614969bc36ee7b5f5988288b47409007361
Hayden Haddock <Hayden Haddock>
2017-09-26 10:45:22 -0500

    db connection

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
   <head>
     <meta charset="utf-8">
     <title>Web Chat Login</title>
+    <?php include_once "php/connection.php" ?>
   </head>
   <body>
     <form class="" action="" method="post">
-      
+
     </form>
   </body>
 </html>
