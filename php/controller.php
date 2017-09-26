0aff52ba5103a14c87219f3cc7acf495708ad1d5
Hayden Haddock <Hayden Haddock>
2017-09-26 11:50:32 -0500

    login

--- php/controller.php
+++ php/controller.php
@@ -1,7 +1,11 @@
 <?php
+  //Error MSGS : TURN OFF BEFORE PROD
   ini_set('display_startup_errors',1);
   ini_set('display_errors',1);
   error_reporting(-1);
+  // END error_get_last
+
+  require "connection.php";
 
   $connection = new webChatDB();
 
