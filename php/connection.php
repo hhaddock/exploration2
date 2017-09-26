e1cf516212bc63af5b22d0810e595093ae706fd0
Hayden Haddock <Hayden Haddock>
2017-09-26 10:44:23 -0500

    First commit

--- php/connection.php
+++ php/connection.php
@@ -1,7 +1,7 @@
 <?php
   $servername = "localhost";
   $username = "root";
-  $password = "Tigersblvd16!";
+  $password = "Tigersblvd16";
 
   $conn = new mysqli($servername, $username, $password);
   // Check connection
