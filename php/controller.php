7ea0a73fd01e59de7f1adbf08483874f640a4fb1
hhaddock <haydenhaddock1@gmail.com>
2017-09-26 14:43:31 -0500

    working on login

--- php/controller.php
+++ php/controller.php
@@ -19,7 +19,6 @@
     if($connection->checkUser($user) == 1){
       if($connection->checkPass($user, $pass) == 1){
        authenticated($user);
-       header("Location: http://ec2-34-209-75-64.us-west-2.compute.amazonaws.com:4321/")
       } else {
        echo "Username or Password was incorrect";
       }
@@ -31,5 +30,6 @@
   function authenticated($user){
    session_start();
    $_SESSION['user'] = $user;
+   header("Location: http://ec2-34-209-75-64.us-west-2.compute.amazonaws.com:4321/")
   }
 ?>
