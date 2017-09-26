a99e9e9f3c4b20f9a195835a7336683d9650f3e1
Hayden Haddock <Hayden Haddock>
2017-09-26 11:31:39 -0500

    login

--- php/connection.php
+++ php/connection.php
@@ -3,7 +3,7 @@
   $username = "root";
   $password = "Tigersblvd16";
 
-  $conn = new mysqli($servername, $username, $password);
+  $conn = mysqli_connect($servername, $username, $password);
   // Check connection
   if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
