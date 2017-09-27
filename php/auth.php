d9bac44049ef7ec04f9f969b17f7e53c8cbbc9f3
Hayden Haddock <Hayden Haddock>
2017-09-27 12:31:37 -0500

    theme test

--- php/auth.php
+++ php/auth.php
@@ -4,7 +4,7 @@
  // echo $auth = $_SESSION['authenticated'];
  // echo "&";
  // echo $user = $_SESSION['user'];
- if(isset($_COOKIE['user'])){
+ if(isset($_SESSION['authenticated'])){
    echo "Cookie user exists ".$_COOKIE['user'];
  } else {
    echo "cookie is not set!";
