60f981b2374f1b089ec2f8769c4e5e60b44c4e51
Hayden Haddock <Hayden Haddock>
2017-09-27 13:41:02 -0500

    theme test

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <head>
     <meta charset="utf-8">
     <title>Web Chat Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/darkly/bootstrap.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
