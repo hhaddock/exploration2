206883be36ac28dab525dd51848625155be56ad3
Hayden Haddock <Hayden Haddock>
2017-09-27 12:11:31 -0500

    theme test

--- php/auth.php
+++ php/auth.php
@@ -1,7 +1,10 @@
 <?php
  session_start();
 
- echo $auth = $_SESSION['authenticated'];
- echo "&";
- echo $user = $_SESSION['user'];
+ // echo $auth = $_SESSION['authenticated'];
+ // echo "&";
+ // echo $user = $_SESSION['user'];
+ if(isset($_COOKIE['user'])){
+   echo "Cookie user exists ".$_COOKIE['user'];
+ }
 ?>
