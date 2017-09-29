d14c2abea942b2bb3f1a7bcea8e394001d308564
Hayden Haddock <Hayden Haddock>
2017-09-29 11:51:44 -0500

    theme test

--- js/funct.js
+++ js/funct.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
 
   $("#loginForm").submit(function(event){
     $(".login").each(function(){
-      console.log($(this).size);
+      console.log($(this).length);
       if($(this).length <= 1){
         alert("Error: you must fill out all form elements");
         event.preventDefault();
